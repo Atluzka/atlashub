378e4eb214eeb9a662e78b05710677b75a6719f7
Atlaz <52002842+Atluzka@users.noreply.github.com>
2022-06-20 08:07:52 +0300

    Add files via upload
    main file

---
+++ atlashub.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 3101667897 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Atluzka/atlashub/main/legendsofspeed.lua"))()
+end
+
+if game.PlaceId == 9681073174 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Atluzka/atlashub/main/gymtrainingsimulator.lua"))()
+end
+
+if game.PlaceId == 9297410766 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Atluzka/atlashub/main/crossbarchallengesimulator.lua"))()
+end
