c481d89543683764596ce822a03dd8eda8edc6e6
Atlaz <52002842+Atluzka@users.noreply.github.com>
2022-06-21 23:27:58 +0300

    Update atlashub.lua

--- atlashub.lua
+++ atlashub.lua
@@ -1,19 +1,27 @@
-if game.PlaceId == 3101667897 then
+if game.PlaceId == 3101667897 then -- Legends Of Speed
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Atluzka/atlashub/main/legendsofspeed.lua"))()
 end
 
-if game.PlaceId == 9681073174 then
+if game.PlaceId == 9681073174 then -- Gym Training Simulator
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Atluzka/atlashub/main/gymtrainingsimulator.lua"))()
 end
 
-if game.PlaceId == 9297410766 then
+if game.PlaceId == 9297410766 then -- Crossbar Challenge Simulator
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Atluzka/atlashub/main/crossbarchallengesimulator.lua"))()
 end
 
-if game.PlaceId == 537413528 then
+if game.PlaceId == 537413528 then -- Build A Boat For Treasure
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Atluzka/atlashub/main/buildaboatfortreasure.lua"))()
 end
 
-if game.PlaceId == 9306912037 then
+if game.PlaceId == 9306912037 then -- Hole In One Simulator
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Atluzka/atlashub/main/holeinonesimulator.lua"))()
 end 
+
+if game.PlaceId == 4787629450 then -- Booga Booga - Classic
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Atluzka/atlashub/main/boogabooga.lua"))() 
+end
+
+if game.PlaceId == 961409234 then -- Blood Moon Tycoon
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Atluzka/atlashub/main/bloodmoontycoon.lua"))() 
+end
